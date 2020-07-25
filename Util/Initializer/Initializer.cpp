582c23cca7d4ec415058026404ba9100b56c4da4
MohamedSayedSalah <mohamed.sayed.salah@outlook.com>
2020-07-25 15:08:51 +0200

    add Initializer

---
+++ Util/Initializer/Initializer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mohamed Sayed on 7/25/20.
+//
+
+#include "Initializer.h"
---
+++ Util/Initializer/Initializer.h
@@ -0,0 +1,14 @@
+//
+// Created by Mohamed Sayed on 7/25/20.
+//
+
+#ifndef RAILSROAD_INITIALIZER_H
+#define RAILSROAD_INITIALIZER_H
+
+
+class Initializer {
+
+};
+
+
+#endif //RAILSROAD_INITIALIZER_H
