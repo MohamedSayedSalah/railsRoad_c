abb70f2f0835db71b12e891d220a4ea179a1e6c8
MohamedSayedSalah <mohamed.sayed.salah@outlook.com>
2020-07-26 03:06:55 +0200

    some clean up

--- tests/RailsRoadTests.cpp
+++ tests/RailsRoadTests.cpp
@@ -25,13 +25,9 @@ namespace {
         }
         void SetUp( ) {
             initializer.setFilePath("../Tests/test1") ;
-            // code here will execute just before the test ensues
+
         }
 
-        void TearDown( ) {
-            // code here will be called just after the test completes
-            // ok to through exceptions from here if need be
-        }
 
     };
 
