dbda6825bb79d9f7f627f2feb94b797d067e4173
MohamedSayedSalah <mohamed.sayed.salah@outlook.com>
2020-07-26 15:44:13 +0200

    remove build folder

--- cmake-build-debug/Tests/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mohamedsayed/CLionProjects/railsRoad/tests/lib/googletest/googlemock
-# Build directory: /Users/mohamedsayed/CLionProjects/railsRoad/cmake-build-debug/tests/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
